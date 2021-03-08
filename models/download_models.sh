c69a6a9b67ec3fa2f947d5520be1ef12c18d9a4b
Eva Wei <mingyue.wei@hotmail.com>
2021-03-08 12:37:52 -0500

    update model download cmd

--- models/download_models.sh
+++ models/download_models.sh
@@ -5,3 +5,7 @@ fi
 if [[ ! -f all_entities_large.t7 ]]; then
     wget http://dl.fbaipublicfiles.com/BLINK/all_entities_large.t7
 fi
+
+if [[ ! -f faiss_hnsw_index.pkl ]]; then
+    wget http://dl.fbaipublicfiles.com/elq/faiss_hnsw_index.pkl
+fi
