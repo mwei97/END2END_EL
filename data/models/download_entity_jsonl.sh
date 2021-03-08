4c982f9ce23d594a65b2f22a6b19fe74c0999c4f
Eva Wei <mingyue.wei@hotmail.com>
2021-03-07 22:36:01 -0500

    add model/entity download cmd

---
+++ data/models/download_entity_jsonl.sh
@@ -0,0 +1,3 @@
+if [[ ! -f entity.jsonl ]]; then
+    wget http://dl.fbaipublicfiles.com/elq/entity.jsonl
+fi
---
+++ models/download_models.sh
@@ -0,0 +1,7 @@
+if [[ ! -f entity_token_ids_128.t7 ]]; then
+    wget http://dl.fbaipublicfiles.com/elq/entity_token_ids_128.t7
+fi
+
+if [[ ! -f all_entities_large.t7 ]]; then
+    wget http://dl.fbaipublicfiles.com/BLINK/all_entities_large.t7
+fi
