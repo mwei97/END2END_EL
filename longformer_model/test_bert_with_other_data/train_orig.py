acdd4712327d38f7e5bbfd2c1ee1a1556299dc66
Eva Wei <mingyue.wei@hotmail.com>
2021-04-02 00:40:08 -0400

    test bert with other code

---
+++ longformer_model/test_bert_with_other_data/train_orig.py
@@ -0,0 +1,200 @@
+from transformers import BertTokenizer, BertForTokenClassification, BertConfig
+from torch.optim import Adam
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import numpy as np
+from tqdm import tqdm, trange
+from sklearn.metrics import confusion_matrix
+
+import utils
+
+
+def get_special_tokens(tokenizer, tag2idx):
+
+    pad_tok = tokenizer.vocab["[PAD]"]
+    sep_tok = tokenizer.vocab["[SEP]"]
+    cls_tok = tokenizer.vocab["[CLS]"]
+    o_lab = tag2idx["O"]
+
+    return pad_tok, sep_tok, cls_tok, o_lab
+
+
+def flat_accuracy(valid_tags, pred_tags):
+
+    """
+    Define a flat accuracy metric to use while training the model.
+    """
+
+    return (np.array(valid_tags) == np.array(pred_tags)).mean()
+
+
+def annot_confusion_matrix(valid_tags, pred_tags):
+
+    """
+    Create an annotated confusion matrix by adding label
+    annotations and formatting to sklearn's `confusion_matrix`.
+    """
+
+    # Create header from unique tags
+    header = sorted(list(set(valid_tags + pred_tags)))
+
+    # Calculate the actual confusion matrix
+    matrix = confusion_matrix(valid_tags, pred_tags, labels=header)
+
+    # Final formatting touches for the string output
+    mat_formatted = [header[i] + "\t" + str(row) for i, row in enumerate(matrix)]
+    content = "\t" + " ".join(header) + "\n" + "\n".join(mat_formatted)
+
+    return content
+
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
+
+    label_types = ['B-PER', 'I-PER', 'B-LOC', 'I-LOC', 'B-ORG', 'I-ORG', 'B-MISC', 'I-MISC', 'O']
+    tag2idx = {t: i for i, t in enumerate(label_types)}
+    idx2tag = {i: t for t, i in tag2idx.items()}
+    NUM_LABELS = len(label_types)
+
+    train_batch_size = eval_batch_size = 32
+    train_dataloader, valid_dataloader = utils.load_and_prepare_data(
+        tokenizer, train_batch_size, eval_batch_size
+    )
+
+    model = BertForTokenClassification.from_pretrained('bert-base-cased', num_labels=NUM_LABELS)
+    model.to(device)
+
+    # todo: lr different than mine
+    optimizer = Adam(model.parameters(), lr=3e-5)
+
+    pad_tok, sep_tok, cls_tok, o_lab = get_special_tokens(tokenizer, tag2idx)
+
+    epochs = 5
+
+    for _ in trange(epochs, desc="Epoch"):
+        epoch += 1
+
+        # Training loop
+        print("Starting training loop.")
+        model.train()
+        tr_loss, tr_accuracy = 0, 0
+        nb_tr_examples, nb_tr_steps = 0, 0
+        tr_preds, tr_labels = [], []
+
+        for step, batch in enumerate(train_dataloader):
+
+            # Add batch to gpu
+            batch = tuple(t.to(device) for t in batch)
+            b_input_ids, b_input_mask, b_labels = batch
+
+            # Forward pass
+            outputs = model(
+                b_input_ids,
+                token_type_ids=None,
+                attention_mask=b_input_mask,
+                labels=b_labels,
+            )
+            loss, tr_logits = outputs[:2]
+
+            # Backward pass
+            loss.backward()
+
+            # Compute train loss
+            tr_loss += loss.item()
+            nb_tr_examples += b_input_ids.size(0)
+            nb_tr_steps += 1
+
+            # Subset out unwanted predictions on CLS/PAD/SEP tokens
+            preds_mask = (
+                (b_input_ids != cls_tok)
+                & (b_input_ids != pad_tok)
+                & (b_input_ids != sep_tok)
+            )
+
+            tr_logits = tr_logits.detach().cpu().numpy()
+            tr_label_ids = torch.masked_select(b_labels, (preds_mask == 1))
+            tr_batch_preds = np.argmax(tr_logits[preds_mask.squeeze()], axis=1)
+            tr_batch_labels = tr_label_ids.cpu().numpy()
+            tr_preds.extend(tr_batch_preds)
+            tr_labels.extend(tr_batch_labels)
+
+            # Compute training accuracy
+            tmp_tr_accuracy = flat_accuracy(tr_batch_labels, tr_batch_preds)
+            tr_accuracy += tmp_tr_accuracy
+
+            # Gradient clipping
+            torch.nn.utils.clip_grad_norm_(
+                parameters=model.parameters(), max_norm=1.0
+            )
+
+            # Update parameters
+            optimizer.step()
+            model.zero_grad()
+
+        tr_loss = tr_loss / nb_tr_steps
+        tr_accuracy = tr_accuracy / nb_tr_steps
+
+        # Print training loss and accuracy per epoch
+        print(f"Train loss: {tr_loss}")
+        print(f"Train accuracy: {tr_accuracy}")
+
+        # Validation loop
+        print("Starting validation loop.")
+
+        model.eval()
+        eval_loss, eval_accuracy = 0, 0
+        nb_eval_steps, nb_eval_examples = 0, 0
+        predictions, true_labels = [], []
+
+        for batch in valid_dataloader:
+
+            batch = tuple(t.to(device) for t in batch)
+            b_input_ids, b_input_mask, b_labels = batch
+
+            # todo: maybe I should change eval here?
+            with torch.no_grad():
+                outputs = model(
+                    b_input_ids,
+                    token_type_ids=None,
+                    attention_mask=b_input_mask,
+                    labels=b_labels,
+                )
+                tmp_eval_loss, logits = outputs[:2]
+
+            # Subset out unwanted predictions on CLS/PAD/SEP tokens
+            preds_mask = (
+                (b_input_ids != cls_tok)
+                & (b_input_ids != pad_tok)
+                & (b_input_ids != sep_tok)
+            )
+
+            logits = logits.detach().cpu().numpy()
+            label_ids = torch.masked_select(b_labels, (preds_mask == 1))
+            val_batch_preds = np.argmax(logits[preds_mask.squeeze()], axis=1)
+            val_batch_labels = label_ids.cpu().numpy()
+            predictions.extend(val_batch_preds)
+            true_labels.extend(val_batch_labels)
+
+            tmp_eval_accuracy = flat_accuracy(val_batch_labels, val_batch_preds)
+
+            eval_loss += tmp_eval_loss.mean().item()
+            eval_accuracy += tmp_eval_accuracy
+
+            nb_eval_examples += b_input_ids.size(0)
+            nb_eval_steps += 1
+
+        # Evaluate loss, acc, conf. matrix, and class. report on devset
+        pred_tags = [idx2tag[i] for i in predictions]
+        valid_tags = [idx2tag[i] for i in true_labels]
+        #cl_report = classification_report(valid_tags, pred_tags)
+        conf_mat = annot_confusion_matrix(valid_tags, pred_tags)
+        eval_loss = eval_loss / nb_eval_steps
+        eval_accuracy = eval_accuracy / nb_eval_steps
+
+        # Report metrics
+        print(f"Validation loss: {eval_loss}")
+        print(f"Validation Accuracy: {eval_accuracy}")
+        #print(f"Classification Report:\n {cl_report}")
+        print(f"Confusion Matrix:\n {conf_mat}")
