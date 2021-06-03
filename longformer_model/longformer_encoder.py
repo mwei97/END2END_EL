35f596799d56ff0ec3ef7c1c4e51b9471dd8b680
Eva Wei <mingyue.wei@hotmail.com>
2021-06-03 18:41:56 +0800

    temporary change to spanbert

--- longformer_model/longformer_encoder.py
+++ longformer_model/longformer_encoder.py
@@ -34,7 +34,9 @@ class LongEncoderModule(nn.Module):
             longformer_output_dim = self.ctxt_encoder.embeddings.word_embeddings.weight.size(1)
             self.NULL_IDX = 0
         else:
-            self.ctxt_encoder = BertModel.from_pretrained('bert-base-uncased')
+            # temporary change to large cased SpanBert for test
+            #self.ctxt_encoder = BertModel.from_pretrained('bert-base-uncased')
+            self.ctxt_encdoer = BertModel.from_pretrained('../models/spanbert_hf')
             self.NULL_IDX = 0
             longformer_output_dim = self.ctxt_encoder.embeddings.word_embeddings.weight.size(1)
         #num_tags = 4 if not self.params['end_tag'] else 5
@@ -151,7 +153,8 @@ class LongEncoderRanker(nn.Module):
         if params['use_longformer']:
             self.tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
         else:
-            self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+            #self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+            self.tokenizer = BertTokenizer.from_pretrained('bert-large-cased', do_lower_case=False)
         #self.pad_id = 0
         self.pad_id = -1
         # init model
